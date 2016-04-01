f99b49f4330a648a99f4ffcce7e190bbe7770d5a
Nathaniel Miller <dukeoflaser@gmail.com>
2016-04-01 03:06:47 +0000

    Required controller files manually in environment.rb as they were not being loaded properly.

--- environment.rb
+++ environment.rb
@@ -2,4 +2,9 @@ require 'bundler'
 
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "./app/controllers", "*.rb")].each {|f| require f}
+# Dir[File.join(File.dirname(__FILE__), "./app/controllers", "*.rb")].each {|f| require f}
+
+require './app/controllers/application_controller'
+require './app/controllers/hello_controller'
+require './app/controllers/registrations_controller'
+require './app/controllers/root_controller'
